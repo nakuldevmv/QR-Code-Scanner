8429682997bc84c4a0e6e457da36379d81f61e0c
Nakul Dev <nakuldev1561@gmail.com>
2024-08-31 15:29:39 +0530

    Update qr.dart

--- lib/qr/qr.dart
+++ lib/qr/qr.dart
@@ -19,42 +19,78 @@ class _qrscannerState extends State<qrscanner> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("QR Code Scanner"),
+          backgroundColor: const Color.fromARGB(255, 29, 13, 34),
+          title: const Text(
+              style: TextStyle(color: Colors.white), "QR Code Scanner"),
           elevation: 0,
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            const Text("Place the QR code below"),
-            Container(
-              height: 300,
-              width: 300,
-              color: Colors.black,
-              child: QRView(key: qrKey, onQRViewCreated: _onQRViewCreated),
-            ),
-            Container(
-              child: (result != null)
-                  ? Text("QR Data : ${result!.code}")
-                  : const Text("Scan a code"),
-            ),
-            Container(
-              child: ElevatedButton(
-                  onPressed: () {}, child: const Text("Open In Bowser")),
-              // child: (result != null &&
-              //         (Uri.parse(result!.code.toString())
-              //                 .scheme
-              //                 .startsWith('http') ||
-              //             Uri.parse(result!.code.toString())
-              //                 .scheme
-              //                 .startsWith('www')))
-              //     ? ElevatedButton(
-              //         onPressed: () => result != null
-              //             ? _urlLaunch(result!.code.toString())
-              //             : null,
-              //         child: const Text("open in browser"))
-              //     : Container(),
-            )
-          ],
+        backgroundColor: const Color.fromARGB(255, 29, 13, 34),
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 131, 124, 136),
+                  Color.fromARGB(255, 29, 13, 34)
+                ],
+                stops: [0, 1],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text("Place the QR code below"),
+              Container(
+                height: 300,
+                width: 300,
+                color: Colors.black,
+                child: QRView(key: qrKey, onQRViewCreated: _onQRViewCreated),
+              ),
+              Container(
+                child: (result != null)
+                    ? Text("QR Data : ${result!.code}")
+                    : const Text("Scan a code"),
+              ),
+              Container(
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(176, 131, 194, 242),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(100),
+                          topRight: Radius.circular(100),
+                          bottomLeft: Radius.circular(60),
+                          bottomRight: Radius.circular(60),
+                        ),
+                      ),
+                      elevation: 10,
+                      shadowColor: const Color.fromARGB(255, 0, 0, 0),
+                      // minimumSize: const Size(60, 60),
+                      // padding:
+                      //     const EdgeInsets.fromLTRB(0, 0, 20, 0), // padding
+                    ),
+                    onPressed: () {},
+                    child: const Text("Open In Bowser")),
+                // child: (result != null &&
+                //         (Uri.parse(result!.code.toString())
+                //                 .scheme
+                //                 .startsWith('http') ||
+                //             Uri.parse(result!.code.toString())
+                //                 .scheme
+                //                 .startsWith('www')))
+                //     ? ElevatedButton(
+                //         onPressed: () => result != null
+                //             ? _urlLaunch(result!.code.toString())
+                //             : null,
+                //         child: const Text("open in browser"))
+                //     : Container(),
+              )
+            ],
+          ),
         ),
       ),
     );
