41376afe927d149d0ed0aa3b0ea3cafa2abbe1cd
Nakul Dev <nakuldev1561@gmail.com>
2024-09-02 15:45:35 +0530

    Update

--- lib/qr/qr.dart
+++ lib/qr/qr.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -13,167 +14,188 @@ class _qrscannerState extends State<qrscanner> {
   final GlobalKey qrKey = GlobalKey(debugLabel: "QR");
   Barcode? result;
   QRViewController? controller;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 29, 13, 34),
+          backgroundColor: const Color.fromARGB(255, 0, 0, 0),
           title: const Text(
               style: TextStyle(color: Colors.white), "QR Code Scanner"),
           elevation: 0,
         ),
-        backgroundColor: const Color.fromARGB(255, 29, 13, 34),
-        body: Container(
-          decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromARGB(255, 131, 124, 136),
-                  Color.fromARGB(255, 29, 13, 34)
-                ],
-                stops: [0, 1],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-              ),
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20), topRight: Radius.circular(20))),
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+        body: ClipRRect(
+          borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+          child: Stack(
             children: [
-              const Column(
+              QRView(key: qrKey, onQRViewCreated: _onQRViewCreated),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Text(
-                      style: TextStyle(
-                          fontSize: 28,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                      "Position QR Code Below"),
-                  Text(
-                      style: TextStyle(
-                          fontSize: 17,
-                          // fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                      "Align the scanner for auto QR scan."),
-                ],
-              ),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Container(
-                  height: 300,
-                  width: 300,
-                  decoration: BoxDecoration(
-                    // border: Border.all(
-                    //   color: Colors.white,
-                    //   width: 2,
-                    // ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ClipRRect(
                     borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Stack(
-                    children: [
-                      QRView(key: qrKey, onQRViewCreated: _onQRViewCreated),
-                      Positioned(
-                        top: 10,
-                        left: 10,
-                        right: 10,
-                        bottom: 10,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Colors.white,
-                              width: 2,
-                            ),
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                        ),
+                    child: Container(
+                      height: 300,
+                      width: 300,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
                       ),
-                      Positioned(
-                        top: 20,
-                        left: 20,
-                        right: 20,
-                        bottom: 20,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Colors.white,
-                              width: 1,
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            top: 0,
+                            left: 0,
+                            child: Container(
+                              width: 50,
+                              height: 50,
+                              decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(20),
+                                ),
+                                border: Border(
+                                  top:
+                                      BorderSide(color: Colors.white, width: 5),
+                                  left:
+                                      BorderSide(color: Colors.white, width: 5),
+                                ),
+                              ),
                             ),
-                            borderRadius: BorderRadius.circular(10),
                           ),
-                        ),
+                          Positioned(
+                            top: 0,
+                            right: 0,
+                            child: Container(
+                              width: 50,
+                              height: 50,
+                              decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  topRight: Radius.circular(20),
+                                ),
+                                border: Border(
+                                  top:
+                                      BorderSide(color: Colors.white, width: 5),
+                                  right:
+                                      BorderSide(color: Colors.white, width: 5),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            bottom: 0,
+                            left: 0,
+                            child: Container(
+                              width: 50,
+                              height: 50,
+                              decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(20),
+                                ),
+                                border: Border(
+                                  bottom:
+                                      BorderSide(color: Colors.white, width: 5),
+                                  left:
+                                      BorderSide(color: Colors.white, width: 5),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            bottom: 0,
+                            right: 0,
+                            child: Container(
+                              width: 50,
+                              height: 50,
+                              decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  bottomRight: Radius.circular(20),
+                                ),
+                                border: Border(
+                                  bottom:
+                                      BorderSide(color: Colors.white, width: 5),
+                                  right:
+                                      BorderSide(color: Colors.white, width: 5),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
-                ),
-              ),
-              Container(
-                child: (result != null)
-                    ? Text("QR Data : ${result!.code}")
-                    : const Text(
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
-                        "Scan a code"),
-              ),
-              Container(
-                // child: ElevatedButton(
-                //     style: ElevatedButton.styleFrom(
-                //       backgroundColor: const Color.fromARGB(176, 131, 194, 242),
-                //       shape: const RoundedRectangleBorder(
-                //         borderRadius: BorderRadius.only(
-                //           topLeft: Radius.circular(100),
-                //           topRight: Radius.circular(100),
-                //           bottomLeft: Radius.circular(60),
-                //           bottomRight: Radius.circular(60),
-                //         ),
-                //       ),
-                //       elevation: 10,
-                //       shadowColor: const Color.fromARGB(255, 0, 0, 0),
-                //       // minimumSize: const Size(60, 60),
-                //       // padding:
-                //       //     const EdgeInsets.fromLTRB(0, 0, 20, 0), // padding
-                //     ),
-                //     onPressed: () {},
-                //     child: const Text("Open In Bowser")),
-                child: (result != null &&
-                        (Uri.parse(result!.code.toString())
-                                .scheme
-                                .startsWith('http') ||
-                            Uri.parse(result!.code.toString())
-                                .scheme
-                                .startsWith('www')))
-                    ? ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor:
-                              const Color.fromARGB(121, 131, 194, 242),
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(100),
-                              topRight: Radius.circular(100),
-                              bottomLeft: Radius.circular(60),
-                              bottomRight: Radius.circular(60),
-                            ),
-                          ),
-                          elevation: 10,
-                          shadowColor: const Color.fromARGB(255, 0, 0, 0),
-                          // minimumSize: const Size(60, 60),
-                          // padding:
-                          //     const EdgeInsets.fromLTRB(0, 0, 20, 0), // padding
-                        ),
-                        onPressed: () => result != null
-                            ? _urlLaunch(result!.code.toString())
-                            : null,
-                        child: const Text(
+                  Container(
+                    child: (result != null)
+                        ? Container(
+                            margin: EdgeInsets.all(5),
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                                color: const Color.fromARGB(113, 0, 0, 0),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10))),
+                            child: Center(
+                              child: SelectableText(
+                                style: TextStyle(color: Colors.white),
+                                " ${result!.code}",
+                              ),
+                            )
+                            //   child: Text(
+                            //       style: TextStyle(color: Colors.white),
+                            //       " ${result!.code}"),
+                            // ),
+                            )
+                        : const Text(
                             style: TextStyle(
-                                fontSize: 30,
+                                fontSize: 20,
                                 fontWeight: FontWeight.bold,
                                 color: Colors.white),
-                            "Open In Browser"))
-                    : Container(),
-              )
+                            "Scan a code"),
+                  ),
+                  Container(
+                    child: (result != null &&
+                            (Uri.parse(result!.code.toString())
+                                    .scheme
+                                    .startsWith('http') ||
+                                Uri.parse(result!.code.toString())
+                                    .scheme
+                                    .startsWith('www')))
+                        ? ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  const Color.fromARGB(209, 16, 28, 36),
+                              shape: const RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10)
+                                        // topLeft: Radius.circular(100),
+                                        // topRight: Radius.circular(100),
+                                        // bottomLeft: Radius.circular(60),
+                                        // bottomRight: Radius.circular(60),
+                                        ),
+                              ),
+                              elevation: 10,
+                              shadowColor: const Color.fromARGB(255, 0, 0, 0),
+                            ),
+                            onPressed: () => result != null
+                                ? _urlLaunch(result!.code.toString())
+                                : null,
+                            child: const Text(
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                                "Open In Browser"))
+                        : Container(),
+//
+//
+                  )
+                ],
+              ),
             ],
           ),
         ),
