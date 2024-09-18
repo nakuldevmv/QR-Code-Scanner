229d5403699c36c54f9d53b737181a594f332ed3
Nakul Dev <nakuldev1561@gmail.com>
2024-09-18 22:33:39 +0530

    update

--- android/app/src/main/kotlin/com/example/qrscanner/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.qrscanner
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ prodigy_ad_05/android/app/src/main/kotlin/com/example/prodigy_ad_05/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.prodigy_ad_05
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
